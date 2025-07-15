08d1c091c08d44706ccfdd2c1558fbde55a69852
panicbaek <woosikbaek@gmail.com>
2025-07-15 23:29:06 +0900

    0715

--- 0715/2.날씨api사용/script2.js
+++ 0715/2.날씨api사용/script2.js
@@ -1,7 +1,11 @@
 const select = document.querySelector('select');
 
-const show = document.querySelector('.show');
+
+const show1 = document.querySelector('.show1');
 const show2 = document.querySelector('.show2');
+const show3 = document.querySelector('.show3');
+const show4 = document.querySelector('.show4');
+const show5 = document.querySelector('.show5');
 
 const countryCoordinates = {
   한국: {
@@ -32,18 +36,37 @@ const countryCoordinates = {
 
 select.addEventListener('change', ()=> {
 
-  console.log(select);
-  const name = select.value;
+  console.log(select)
+  const name = select.value
   const lat = countryCoordinates[name].latitude;
-  const lon = countryCoordinates[name].longitude;
+  const lon = countryCoordinates[name].longitude
   const APIKEY = '57388f5537da4cf7daca539f7fe4a995'
-  const URL = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=$${lon}&exclude={part}&appid=${APIKEY}`
-  console.log(URL)
+  const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIKEY}`
+  
+  fetch(URL)
+    .then( response => response.json())
+    .then(data => {
+      console.log(data)
+      
+      const main = data.weather[0].main
+      const id = data.weather[0].id
+      const description = data.weather[0].description
+      const feels = data.main.feels_like;
+      const clouds = data.clouds.all
+
+
+      show1.innerHTML = main;
+      show2.innerHTML = id;
+      show3.innerHTML = description;
+      show4.innerHTML = feels;
+      show5.innerHTML = clouds;
+      
+
+    })
 
   
+  
 
-  show.innerHTML = countryCoordinates[name].latitude;
-  show2.innerHTML = countryCoordinates[name].longitude;
 
 })
 
